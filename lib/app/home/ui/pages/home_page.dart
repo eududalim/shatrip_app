a1e55c3dd5da69a1b7e5886db8c3381696818e11
Eduarda Lima <eududalim@gmail.com>
2021-11-21 20:07:22 -0300

    Criaçao do search do home

--- lib/app/home/ui/pages/home_page.dart
+++ lib/app/home/ui/pages/home_page.dart
@@ -11,56 +11,95 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: [
           HomeBackground(),
-          Column(
-            children: [
-              SizedBox(height: 70),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,                      
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[300],
+          SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  //   color: Colors.indigo.withAlpha(255),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[300],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height > 640 ? 30 : 8
-                    ),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Flexible(
-                            flex: 1,
-                            child: Container(
-                              child: Text('De:', style: HomeTheme.homeTextSearch1,),
-                            ),
-                          ),
-                          Flexible(
-                            flex: 2,
-                            child: Container(  
-                              padding: EdgeInsets.all(4),
-                             decoration: BoxDecoration(
-                               border: Border.all(
-                                color: Colors.white
-                               )
-                             ),                          
-                              child: Row(
-                                children: [
-                                  Icon(Icons.location_pin, color: Colors.white, size: 16,),
-                                  Text('Carinhanha', style: HomeTheme.homeTextSearch1,)
-                                ],
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height > 640
+                              ? 30
+                              : 8),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                child: Text(
+                                  'De:',
+                                  style: HomeTheme.homeTextSearch1,
+                                ),
                               ),
                             ),
-                          )
-                        ],
+                            Flexible(
+                              flex: 2,
+                              child: Container(
+                                padding: EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.white)),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.location_pin,
+                                      color: Colors.white,
+                                      size: 16,
+                                    ),
+                                    Text(
+                                      'Carinhanha',
+                                      style: HomeTheme.homeTextSearch1,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        height:
+                            MediaQuery.of(context).size.height > 640 ? 24 : 8,
+                      ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                child: Text(
+                                  'Para:',
+                                  style: HomeTheme.homeTextSearch1,
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                                flex: 2,
+                                child: Container(
+                                  height: 31,
+                                  child: TextField(
+                                    style: HomeTheme.homeTextSearch1,
+                                    decoration: HomeTheme.inputDecoration1(hintText: 'Para onde deseja ir?')
+                                  ),
+                                ))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
--- lib/app/home/ui/themes/home_theme.dart
+++ lib/app/home/ui/themes/home_theme.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class HomeTheme {
-  
   static TextStyle get homeTextSearch1 => TextStyle(
         fontSize: 16,
         // fontWeight: FontWeight.w100,
         color: Colors.white,
+      );
+
+  static InputDecoration inputDecoration1({String hintText, Icon icon, String labelText}) =>
+      InputDecoration(
+        labelText: labelText,
+        prefixIcon: icon?? null,
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Colors.white60,
+          fontWeight: FontWeight.w400,
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.white,
+        )),
+        contentPadding: EdgeInsets.symmetric(horizontal: 14, vertical: 4),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white70,
+          ),
+        ),
       );
 }
